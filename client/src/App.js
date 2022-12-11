581a7bfd95a5518e0700f8d2aa41dc56f98baf81
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2022-12-11 22:28:20 +0100

    Παιγνίδι με το Bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Form } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <h1 className="title">ΟΥΛΕ ΤΕ ΚΑΙ ΜΑΛΑ ΧΑΙΡΕ</h1>
+      <body className='App'>
+        <header className='App-header'>
+          <h1 className='h1'>ΟΥΛΕ ΤΕ ΚΑΙ ΜΑΛΑ ΧΑΙΡΕ</h1>
+          <Button>Δοκιμαστικό</Button>
+
+          <Form>
+            <Form.Group>
+              <Form.Label>Εγγραφείτε</Form.Label>
+              <Form.Control type='email' placeholder='πχ παράδειγμα@icloud.com'></Form.Control>
+              <Form.Text className='text-muted'>Τα δεδομένα σας είναι ασφαλή μαζί μας</Form.Text>
+            </Form.Group>
+          </Form>
+        </header>
+      </body>
   );
 }
 
