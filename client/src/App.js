64c013252b1b3e1f89ef5758a2b5f0722d17399a
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-11-06 20:25:42 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
-import { Button, Form } from 'react-bootstrap'
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
       <body className='App'>
-        <header className='App-header'>
+        <Navbar bg="secondary" expand="lg">
+            <Container>
+              <Navbar.Brand href="#home">ΑΤΑ</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                  <Nav.Link href="#home">Αρχική</Nav.Link>
+                  <Nav.Link href="#link">Link</Nav.Link>
+                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+        <footer className='App-header'>
+          
           <h1 className='h1'>ΟΥΛΕ ΤΕ ΚΑΙ ΜΑΛΑ ΧΑΙΡΕ</h1>
+          
           <Button>Δοκιμαστικό</Button>
 
           <Form>
@@ -16,7 +38,8 @@ function App() {
               <Form.Text className='text-muted'>Τα δεδομένα σας είναι ασφαλή μαζί μας</Form.Text>
             </Form.Group>
           </Form>
-        </header>
+          
+        </footer>
       </body>
   );
 }
